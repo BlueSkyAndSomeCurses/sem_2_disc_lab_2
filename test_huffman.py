d7580b48f551a483feaf49b6ad49c1ed3c8db73c
Wouch <noreply@noreply.xyz>
2024-03-06 11:51:49 +0200

    coding works

--- test_huffman.py
+++ test_huffman.py
@@ -16,16 +16,30 @@ class Huffman:
             new_entry.sort(key=lambda pair: pair[1], reverse=True)
             tree_hist.append(new_entry)
 
-        coding_dict = {tree_hist[-1][0]: 1, tree_hist[-1][1]: 0}
+        coding_dict = {tree_hist[-1][0]: "1", tree_hist[-1][1]: "0"}
 
         tree_hist = tree_hist[::-1][1:]
 
         for node in tree_hist:
             for key, value in coding_dict.items():
                 if key not in node:
-                    print(key, node)
 
-        return coding_dict
+                    for pair in node:
+                        if pair[0] == key[0][0]:
+                            left_prob = pair[1]
+                        elif pair[0] == key[0][1]:
+                            right_prob = pair[1]
+
+                    print(left_prob)
+                    coding_dict[(key[0][0], left_prob)] = value + "1"
+                    coding_dict[(key[0][1], right_prob)] = value + "0"
+                    coding_dict.pop(key)
+                    break
+
+        for char, code in coding_dict.items():
+            text = text.replace(char[0], code)
+
+        return text, coding_dict
 
     def decode(self, code: str, coding_dict: dict[str, str]):
         pass
@@ -34,5 +48,5 @@ class Huffman:
 text = "asdfasdfdfsfaaaaafsdaaa"
 
 haffman = Huffman()
-encoded = haffman.encode(text)
+encoded, coding_dict = haffman.encode(text)
 print(encoded)
